9c4aea9ad633fae14fa89635f8d7c95d79ccd2ff
bwehlin <bwehlin@kth.se>
2025-05-26 21:15:29 +0200

    wip

--- Mathlib/Probability/PointProcess/Basic.lean
+++ Mathlib/Probability/PointProcess/Basic.lean
@@ -31,6 +31,22 @@ namespace Probability.RandomMeasures
 --variable {Ω : Type*} [MeasurableSpace Ω]
 --variable {E : Type*} [MeasurableSpace E]
 
+@[class] structure PointProcess (Ω E : Type*) [MeasurableSpace Ω] [MeasurableSpace E] (μ : Measure Ω) [IsProbabilityMeasure μ] where
+  rv : Ω → Measure E
+  is_point_measure_as : μ {ω | IsPointMeasure (rv ω)} = 1
+
+def evaluation_map (Ω E : Type*) [MeasurableSpace Ω] [MeasurableSpace E] (μ : Measure Ω) [IsProbabilityMeasure μ]
+  (N : PointProcess Ω E μ) (a : Set E) : Ω → EReal := (fun ν => ν a) ∘ N.rv
+
+instance NormedAddCommGroup : EReal := sorry
+
+def intensity_measure (Ω E : Type*) [MeasureSpace Ω] [MeasurableSpace E] (μ : Measure Ω) [IsProbabilityMeasure μ]
+  (N : PointProcess Ω E μ) (a : Set E) := 𝔼[evaluation_map Ω E μ N a]
+
+def IsStationary (Ω E : Type*) [MeasurableSpace Ω] [MeasurableSpace E] (μ : Measure Ω) [IsProbabilityMeasure μ]
+  (N : PointProcess Ω E μ) (a : Set E) : 𝔼[N.rv a] = 1 Prop :=
+  sorry
+
 variable {α β δ : Type*} [MeasurableSpace α] [MeasurableSpace β] {s : Set α} {a : α}
 
 variable {Ω E : Type*} [MeasurableSpace Ω] [MeasurableSpace E]
